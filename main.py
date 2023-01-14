dc4000fb2e63ed9e6635172dff98afe0d44288c0
Msalehi237 <m.salehi23771@gmail.com>
2023-01-13 21:31:23 -0600

    added

--- main.py
+++ main.py
@@ -260,6 +260,7 @@ def test(test_loader, tnet):
     return total
 
 
+
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     """Saves checkpoint to disk"""
     directory = "runs/%s/" % (args.name)
